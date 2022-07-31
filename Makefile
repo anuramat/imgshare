092488170bec44b1bbf57c5dc07e4a34054f3521
Arsen Nuramatov <anuramat@pm.me>
2022-07-31 23:42:14 +0300

    [GOHW-2]: added gateway -> rest api and openapi

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
-.PHONY: run build deps compile
-gen: deps
+.PHONY: run build3 compile gen .dep
+gen: .dep
 	protoc --go_opt=paths=source_relative --go-grpc_opt=paths=source_relative --go_out=internal --go-grpc_out=internal api/api.proto
+
+	protoc -I . --grpc-gateway_out ./internal \
+    --grpc-gateway_opt logtostderr=true \
+    --grpc-gateway_opt paths=source_relative \
+    --grpc-gateway_opt generate_unbound_methods=true \
+    api/api.proto
+
+	protoc -I . --openapiv2_out ./openapi \
+    --openapiv2_opt logtostderr=true \
+ 	api/api.proto
 
 run: gen
 	go run cmd/bot/main.go
@@ -12,6 +22,9 @@ build: compile
 	go build -o bin/bot cmd/bot/main.go
 	go build -o bin/db cmd/db/main.go
 
-deps:
-	go install google.golang.org/protobuf/cmd/protoc-gen-go@v1.28
-	go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@v1.2
+LOCAL_GOPATH:=$(CURDIR)/BIN
+.dep: 
+	GOBIN=$(LOCAL_GOPATH) go install github.com/grpc-ecosystem/grpc-gateway/v2/protoc-gen-grpc-gateway && \
+	GOBIN=$(LOCAL_GOPATH) go install google.golang.org/protobuf/cmd/protoc-gen-go && \
+	GOBIN=$(LOCAL_GOPATH) go install google.golang.org/grpc/cmd/protoc-gen-go-grpc
+
--- cmd/db/main.go
+++ cmd/db/main.go
@@ -3,28 +3,55 @@ package main
 import (
 	"log"
 	"net"
+	"net/http"
 
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"gitlab.ozon.dev/anuramat/homework-1/internal/api"
 	"gitlab.ozon.dev/anuramat/homework-1/internal/db"
+	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 const (
-	port = ":51234"
+	port_grpc = ":51234"
+	port_rest = ":5123"
 )
 
 func main() {
 	log.Println("Starting in-memory storage")
+	start_grpc()
+	start_rest()
+}
 
-	listener, err := net.Listen("tcp", port)
+func start_grpc() {
+	listener, err := net.Listen("tcp", port_grpc)
 	if err != nil {
-		log.Panicln("Error starting @ port", port, ":", err)
+		log.Panicln("Error listening on GRPC port", port_grpc, ":", err)
 	}
 	s := grpc.NewServer()
 	api.RegisterBotDBServer(s, db.NewServer())
 	reflection.Register(s)
 	if err := s.Serve(listener); err != nil {
-		log.Panicln("Error serving", err)
+		log.Panicln("Error serving GRPC", err)
+	}
+}
+
+func start_rest() {
+	mux := runtime.NewServeMux()
+	err := api.RegisterBotDBHandlerFromEndpoint(context.Background(), mux, port_grpc, []grpc.DialOption{grpc.WithInsecure()})
+	if err != nil {
+		log.Panicln("Error connecting gateway to GRPC", err)
+	}
+	server := http.Server{
+		Handler: mux,
+	}
+	l, err := net.Listen("tcp", port_rest)
+	if err != nil {
+		log.Panicln("Error listening on REST port", port_rest, err)
+	}
+	err = server.Serve(l)
+	if err != nil {
+		log.Panicln("Error serving REST")
 	}
 }
