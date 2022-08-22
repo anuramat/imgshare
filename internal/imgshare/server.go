a71005693e7ba4ecd5b03410259140c907240c72
Arsen Nuramatov <anuramat@pm.me>
2022-08-22 23:19:06 +0300

    [GOHW-5]: removed dead code

--- internal/imgshare/server.go
+++ internal/imgshare/server.go
@@ -3,27 +3,19 @@ package imgshare
 import (
 	"context"
 	"os"
-	"sync"
 
 	"github.com/jackc/pgx/v4/pgxpool"
 	"gitlab.ozon.dev/anuramat/homework-1/internal/api"
 )
 
 type Server struct {
-	messageFiles MessageFiles
 	api.UnimplementedImgShareServer
 	pool   chan struct{}
 	DBPool *pgxpool.Pool
 }
 
-type MessageFiles struct {
-	mu   *sync.RWMutex
-	data map[int64]string
-}
-
 func NewServer(n_jobs int) (s *Server, err error) {
 	s = &Server{}
-	s.messageFiles = MessageFiles{&sync.RWMutex{}, make(map[int64]string)}
 	s.pool = make(chan struct{}, n_jobs)
 	s.DBPool, err = pgxpool.Connect(context.Background(), getDBURL())
 	return
