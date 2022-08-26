096622e476befbff24b0fb8d9844c5f67df56230
Arsen Nuramatov <anuramat@pm.me>
2022-08-26 18:28:05 +0300

    messages tests done (regular grpc errors)

--- internal/messages/messages_test.go
+++ internal/messages/messages_test.go
@@ -2,6 +2,7 @@ package messages
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -21,6 +22,10 @@ func makeMsg(uid int64, cid int64, fid string, text string) *tgbotapi.Message {
 	}
 }
 
+var (
+	Err = errors.New("")
+)
+
 func TestUploadImageHandler(t *testing.T) {
 	t.Run("good input", func(t *testing.T) {
 		// arrange
@@ -39,6 +44,29 @@ func TestUploadImageHandler(t *testing.T) {
 		// assert
 		assert.Equal(t, data.Users[uid].LastUpload, fid)
 		assert.Equal(t, data.Users[uid].State, models.UploadDescriptionState)
+	})
+
+	t.Run("grpc error case", func(t *testing.T) {
+		// arrange
+		var uid int64 = 123
+		var cid int64 = 456
+
+		msg := &tgbotapi.Message{
+			Photo: []tgbotapi.PhotoSize{},
+			From:  &tgbotapi.User{ID: uid},
+			Chat:  &tgbotapi.Chat{ID: cid},
+			Text:  "",
+		}
+		ctx := context.Background()
+		data := models.NewBotData(nil)
+		data.AddUser(uid)
+		data.Users[uid].State = models.UploadImageState
+
+		// act
+		_ = UploadImageHandler(ctx, msg, data)
+
+		// assert
+		assert.Equal(t, data.Users[uid].State, models.UploadImageState)
 	})
 }
 
@@ -66,6 +94,31 @@ func TestUploadDescriptionHandler(t *testing.T) {
 		// assert
 		assert.Equal(t, data.Users[uid].State, models.NoState)
 	})
+
+	t.Run("grpc error case", func(t *testing.T) {
+		// arrange
+		var uid int64 = 123
+		var cid int64 = 456
+		var fid string = "789"
+		var text string = "xdfasdf"
+
+		msg := makeMsg(uid, cid, "", text)
+		ctx := context.Background()
+		mockCtrl := gomock.NewController(t)
+		mockClient := mocks.NewMockImgShareClient(mockCtrl)
+		req := api.ImageAuthRequest{UserID: msg.From.ID, Image: &api.Image{FileID: fid, Description: msg.Text}}
+		mockClient.EXPECT().CreateImage(ctx, gomock.Eq(&req)).Return(nil, Err)
+		data := models.NewBotData(mockClient)
+		data.AddUser(uid)
+		data.Users[msg.From.ID].State = models.UploadDescriptionState
+		data.Users[msg.From.ID].LastUpload = fid
+
+		// act
+		_ = UploadDescriptionHandler(ctx, msg, data)
+
+		// assert
+		assert.Equal(t, data.Users[uid].State, models.UploadDescriptionState)
+	})
 }
 
 func TestEditDescriptionHandler(t *testing.T) {
@@ -84,6 +137,7 @@ func TestEditDescriptionHandler(t *testing.T) {
 		mockClient.EXPECT().SetDescriptionImage(ctx, gomock.Eq(&req)).Return(nil, nil)
 		data := models.NewBotData(mockClient)
 		data.AddUser(uid)
+		data.Users[msg.From.ID].State = models.EditDescriptionState
 		data.Users[msg.From.ID].LastUpload = fid
 
 		// act
@@ -91,6 +145,31 @@ func TestEditDescriptionHandler(t *testing.T) {
 
 		// assert
 		assert.Equal(t, data.Users[uid].State, models.NoState)
+	})
+
+	t.Run("grpc error case", func(t *testing.T) {
+		// arrange
+		var uid int64 = 123
+		var cid int64 = 456
+		var fid string = "789"
+		var text string = "xdfasdf"
+
+		msg := makeMsg(uid, cid, "", text)
+		ctx := context.Background()
+		mockCtrl := gomock.NewController(t)
+		mockClient := mocks.NewMockImgShareClient(mockCtrl)
+		req := api.ImageAuthRequest{UserID: msg.From.ID, Image: &api.Image{FileID: fid, Description: msg.Text}}
+		mockClient.EXPECT().SetDescriptionImage(ctx, gomock.Eq(&req)).Return(nil, Err)
+		data := models.NewBotData(mockClient)
+		data.AddUser(uid)
+		data.Users[msg.From.ID].State = models.EditDescriptionState
+		data.Users[msg.From.ID].LastUpload = fid
+
+		// act
+		_ = EditDescriptionHandler(ctx, msg, data)
+
+		// assert
+		assert.Equal(t, data.Users[uid].State, models.EditDescriptionState)
 	})
 }
 
@@ -112,11 +191,31 @@ func TestRandomImageHandler(t *testing.T) {
 		data.AddUser(uid)
 
 		// act
-		photo := RandomImageHandler(ctx, msg, data)
+		chattable := RandomImageHandler(ctx, msg, data)
 
 		// assert
 		assert.Equal(t, data.Users[uid].LastDownload, fid)
-		assert.IsType(t, tgbotapi.PhotoConfig{}, photo)
+		assert.IsType(t, tgbotapi.PhotoConfig{}, chattable)
+	})
+	t.Run("grpc error case", func(t *testing.T) {
+		// arrange
+		var uid int64 = 123
+		var cid int64 = 456
+
+		msg := makeMsg(uid, cid, "", "")
+		ctx := context.Background()
+		mockCtrl := gomock.NewController(t)
+		mockClient := mocks.NewMockImgShareClient(mockCtrl)
+		req := api.Empty{}
+		mockClient.EXPECT().GetRandomImage(ctx, gomock.Eq(&req)).Return(nil, Err)
+		data := models.NewBotData(mockClient)
+		data.AddUser(uid)
+
+		// act
+		chattable := RandomImageHandler(ctx, msg, data)
+
+		// assert
+		assert.IsType(t, tgbotapi.MessageConfig{}, chattable)
 	})
 }
 
@@ -148,5 +247,28 @@ func TestGalleryHandler(t *testing.T) {
 		assert.Equal(t, data.Users[uid].LastGalleryIndex, offset)
 		assert.Equal(t, data.Users[uid].LastDownload, fid)
 		assert.IsType(t, tgbotapi.PhotoConfig{}, photo)
+	})
+
+	t.Run("good input", func(t *testing.T) {
+		// arrange
+		var uid int64 = 123
+		var cid int64 = 456
+		var last_index int = 1453
+
+		msg := makeMsg(uid, cid, "", "")
+		ctx := context.Background()
+		mockCtrl := gomock.NewController(t)
+		mockClient := mocks.NewMockImgShareClient(mockCtrl)
+		req := api.GalleryRequest{Offset: int32(last_index), UserID: uid}
+		mockClient.EXPECT().GetGalleryImage(ctx, gomock.Eq(&req)).Return(nil, Err)
+		data := models.NewBotData(mockClient)
+		data.AddUser(uid)
+		data.Users[uid].LastGalleryIndex = last_index
+
+		// act
+		chattable := GalleryHandler(ctx, msg, data)
+
+		// assert
+		assert.IsType(t, tgbotapi.MessageConfig{}, chattable)
 	})
 }
